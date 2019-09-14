0160549c7f2a7e54cb9ca6c712e5415414150bdf
Meagan Utsey <meagan.utsey@gmail.com>
2019-09-14 20:10:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,4 @@
-if "good morning grandma".downcase
+if greeting = "good morning grandma".downcase
 #=> "good morning grandma" 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!

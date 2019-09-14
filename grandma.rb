f6f56d294cd71444555b4d566f27d05de486b854
Meagan Utsey <meagan.utsey@gmail.com>
2019-09-14 20:17:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,7 @@ if greeting == "good morning grandma".downcase
 #=> "good morning grandma" 
 puts "HUH?! SPEAK UP, SONNY"
 elseif greeting =="GOOD MORNING GRANDMA".upcase 
-
-#puts "NO, NOT SINCE 1938!""
+puts "NO, NOT SINCE 1938!""
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'

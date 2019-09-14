082ca63cd18850843d7cb844db5c18ca862e7fc3
Meagan Utsey <meagan.utsey@gmail.com>
2019-09-14 20:20:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,5 +4,5 @@ puts "HUH?! SPEAK UP, SONNY"
 elseif greeting =="GOOD MORNING GRANDMA".upcase 
 puts "NO, NOT SINCE 1938!""
 
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+else greeting == "I LOVE YOU GRANDMA!".upcase
+puts "I LOVE YOU TOO PUMPKIN!".upcase
